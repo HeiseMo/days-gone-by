9498b5cbe603ddeedb61986410ba712f09a8d2a6
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-04 17:04:20 +0200

    Basic layout designed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+///Footer Functions
+
+//Restart Game
+document.getElementById("restartGame").onclick = function () {
+    location.reload();
+};
+
+//Github
+document.getElementById("github").onclick = function () {
+    window.open('https://github.com/HeiseMo/days-gone-by');
+};
+
+
+//Switch
 document.getElementById("switch").onclick = function () {
     let switchMode = document.getElementById("switch").innerHTML
     if (switchMode == '_darkMode') {
@@ -6,6 +20,12 @@ document.getElementById("switch").onclick = function () {
         lightsOn()
     }
 };
+
+//Share
+document.getElementById("share").onclick = function () {
+    //Going to work this in later stage
+};
+
 
 function lightsOn() {
     document.getElementById("switch").innerHTML = "_darkMode";
@@ -16,3 +36,4 @@ function lightsOff() {
     document.getElementById("switch").innerHTML = "_lightMode";
     document.getElementById("mode").className = 'darkbg'
 };
+/// Footer Functions End
