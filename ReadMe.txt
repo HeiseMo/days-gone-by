fe4e7c3f6185215ec289a4687daff4704767310d
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-06 15:16:23 +0200

    game start screen  set up and functioning

---
+++ ReadMe.txt
@@ -0,0 +1,40 @@
+Name: 
+Days Gone By
+
+Idea for the game in its shortest form: 
+
+-Clicker game where you must complete certain actions to survive until help come along
+
+Story:
+
+-You are in a plane crash and the sole survivor of the incident, when you get to yourself you notice
+you arent in the plane anymore, all you see is a forest.
+
+The premise of the game:
+
+-To click action buttons to collect resources to survive the crash(like collecting wood for a fire, food for hunger)
+
+Iteration 1:
+
+- Start screen with the story, explanations for the game and the goal. 
+- Create status for: 
+	health: health of the player out of 100, if it hits 0 game over, dependant on bodyTemperature and hunger status starving
+	hunger: maintained by eating food, low hunger = changes to starving status and actions will be slower, will be out of 100
+	bodyTemperature; maintained by feeding the fire with wood,
+	wood: can be harvested from the forest, will cost energy,
+	food: can be harvested by hunting, will cost energy,
+	daysPassed: total days passed, 
+- Create a days counter for days passing.
+- Create status bar for; health,hunger,bodyTemperature
+- Create a wood collecting system
+- Create a lightFire system
+- Create a function that will update the text-reader(story board) with actions taken
+
+Iteration 2
+
+- Create environment(rooms), one camp site(room) and one river(room), depending on the room different actions can be taken depending on player status.
+- The rooms will be objects with pre defined resources in them.
+- Create object for weapons and traps.
+- Create a food collection system connected to the weapons and trap available.
+- Improve health status by including hunger
+
--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
+//
+let i = 1;
+const txtReaderList = document.querySelectorAll(".textReader li");
+document.getElementById("actionTest").onclick = function () {
+  document.querySelector(".gameScreen span").innerText = i++;
+  if (txtReaderList[0].innerText === "") {
+    txtReaderList[0].innerText = "I was clicked";
+    console.log(txtReaderList[0].innerText);
+  }
+};
+//console.log(txtReaderList[0])
+
 ///Footer Functions
 
 //Restart Game
 document.getElementById("restartGame").onclick = function () {
-    location.reload();
+  location.reload();
 };
 
 //Github
 document.getElementById("github").onclick = function () {
-    window.open('https://github.com/HeiseMo/days-gone-by');
+  window.open("https://github.com/HeiseMo/days-gone-by");
 };
-
 
 //Switch
 document.getElementById("switch").onclick = function () {
-    let switchMode = document.getElementById("switch").innerHTML
-    if (switchMode == '_darkMode') {
-        lightsOff()
-    } else if (switchMode == '_lightMode') {
-        lightsOn()
-    }
+  let switchMode = document.getElementById("switch").innerHTML;
+  if (switchMode == "darkMode") {
+    lightsOff();
+  } else if (switchMode == "lightMode") {
+    lightsOn();
+  }
 };
 
 //Share
-document.getElementById("share").onclick = function () {
-    //Going to work this in later stage
+document.getElementById("play-button").onclick = function () {
+  //Going to work this in later stage
+  document
+  .querySelector(".startGame")
+  .classList.toggle("startGameHidden");
+  document
+    .querySelector(".globalScreenHidden")
+    .classList.toggle("globalScreen");
+  console.log("clicked");
 };
-
 
 function lightsOn() {
-    document.getElementById("switch").innerHTML = "_darkMode";
-    document.getElementById("mode").className = 'lightbg'
-};
+  document.getElementById("switch").innerHTML = "darkMode";
+  document.getElementById("mode").className = "lightbg";
+}
 
 function lightsOff() {
-    document.getElementById("switch").innerHTML = "_lightMode";
-    document.getElementById("mode").className = 'darkbg'
-};
+  document.getElementById("switch").innerHTML = "lightMode";
+  document.getElementById("mode").className = "darkbg";
+}
 /// Footer Functions End
